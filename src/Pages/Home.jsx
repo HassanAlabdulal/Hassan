c4b28c3fba546af26c28a195b01f1caa3294ad4b
HassanAlabdulal <hhhalabdulaal@gmail.com>
2024-01-12 21:16:13 +0300

    feat: home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
-    <section className="h-screen bg-black ">
-      <h1>Hassan</h1>
+    <section className="flex items-center justify-between max-h-screen pt-24 px-96">
+      <div>
+        <h1 className="text-6xl font-semibold tracking-wider text-center text-black">
+          Hassan <br /> Alabdulal
+        </h1>
+      </div>
+
+      <div></div>
     </section>
   );
 }
