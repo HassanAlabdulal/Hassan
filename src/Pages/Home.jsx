3ebe42519872cd1f661761e827df8c8331fa6096
HassanAlabdulal <hhhalabdulaal@gmail.com>
2024-01-12 21:52:31 +0300

    add: Simple TypeWriter Effect

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,27 @@
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+
 export default function Home() {
+  const [text] = useTypewriter({
+    words: ["Software Engineer", "Frontend Web Developer"],
+    loop: {},
+  });
+
   return (
     <section className="flex items-center justify-between max-h-screen pt-24 px-96">
-      <div>
-        <h1 className="text-6xl font-bold leading-tight tracking-wider text-center text-black font-nunito ">
+      <div className="flex flex-col w-1/2">
+        <h1 className="mb-12 text-6xl font-bold leading-tight tracking-wider text-center text-black font-nunito ">
           Hassan <br /> Alabdulal
         </h1>
-      </div>
 
-      <div></div>
+        <div>
+          <h1 className="text-4xl font-semibold leading-tight tracking-wider text-center text-black font-nunito ">
+            {text}
+            <span className="text-red">
+              <Cursor cursorColor="red" />
+            </span>
+          </h1>
+        </div>
+      </div>
     </section>
   );
 }
