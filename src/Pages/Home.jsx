696949a9fad1e57d82d3e6deab6f9e412d12953e
HassanAlabdulal <hhhalabdulaal@gmail.com>
2024-01-12 21:17:59 +0300

    add: new font

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <section className="flex items-center justify-between max-h-screen pt-24 px-96">
       <div>
-        <h1 className="text-6xl font-semibold tracking-wider text-center text-black">
+        <h1 className="text-6xl font-semibold tracking-wider text-center text-black font-nunito">
           Hassan <br /> Alabdulal
         </h1>
       </div>
